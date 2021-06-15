6115f9232c8128b2937e8a015c478e64459cb5c2
n-kremeris <n.kremeris@live.com>
2021-06-15 19:36:45 +0100

    Add makefile and changes to tb.

---
+++ Makefile
@@ -0,0 +1,44 @@
+# Verilator example makefile
+# Norbertas Kremeris 2021
+MODULE=alu
+
+.PHONY:sim
+sim: waveform.vcd
+
+.PHONY:verilate
+verilate: .stamp.verilate
+
+.PHONY:build
+build: obj_dir/Valu
+
+.PHONY:waves
+waves: waveform.vcd
+	@echo
+	@echo "### WAVES ###"
+	gtkwave waveform.vcd
+
+waveform.vcd: ./obj_dir/V$(MODULE)
+	@echo
+	@echo "### SIMULATING ###"
+	@./obj_dir/Valu
+
+./obj_dir/V$(MODULE): .stamp.verilate
+	@echo
+	@echo "### BUILDING SIM ###"
+	make -C obj_dir -f V$(MODULE).mk V$(MODULE)
+
+.stamp.verilate: $(MODULE).sv tb_$(MODULE).cpp
+	@echo
+	@echo "### VERILATING ###"
+	verilator -Wall --trace -cc $(MODULE).sv --exe tb_$(MODULE).cpp
+	@touch .stamp.verilate
+
+.PHONY:lint
+lint: $(MODULE).sv
+	verilator --lint-only $(MODULE).sv
+
+.PHONY: clean
+clean:
+	rm -rf .stamp.*;
+	rm -rf ./obj_dir
+	rm -rf waveform.vcd
--- tb_alu.cpp
+++ tb_alu.cpp
@@ -9,23 +9,53 @@
 
 #define MAX_SIM_TIME 20
 vluint64_t sim_time = 0;
+vluint64_t posedge_cnt = 0;
 
 int main(int argc, char** argv, char** env) {
-	Valu *dut = new Valu;
+//    Verilated::commandArgs(argc, argv);
+
+    Valu *dut = new Valu;
 
     Verilated::traceEverOn(true);
     VerilatedVcdC *m_trace = new VerilatedVcdC;
-	dut->trace(m_trace, 5);
-	m_trace->open("waveform.vcd");
+    dut->trace(m_trace, 5);
+    m_trace->open("waveform.vcd");
 
-	while (sim_time < MAX_SIM_TIME) {
-		dut->clk ^= 1;
-		dut->eval();
-		m_trace->dump(sim_time);
-		sim_time++;
-	}
+    while (sim_time < MAX_SIM_TIME) {
+/*
+        dut->rst = 0;
+        if(sim_time > 3 && sim_time < 6){
+            dut->rst = 1;
+            dut->a_in = 0;
+            dut->b_in = 0;
+            dut->op_in = 0;
+            dut->in_valid = 0;
+        }
+*/
+        dut->clk = 0;
+        dut->eval();
+        m_trace->dump(sim_time);
+        sim_time++;
 
-	m_trace->close();
-	delete dut;
-	exit(EXIT_SUCCESS);
+        dut->clk = 1;
+        dut->eval();
+        posedge_cnt++;
+
+        dut->in_valid = 0;
+        if(posedge_cnt == 5){
+            dut->in_valid = 1;
+        }
+        if(posedge_cnt == 7){
+            if (dut->out_valid != 1)
+                std::cout << "ERROR!" << std::endl;
+        }
+
+        m_trace->dump(sim_time);
+        sim_time++;
+
+    }
+
+    m_trace->close();
+    delete dut;
+    exit(EXIT_SUCCESS);
 }
