31d38c7f511f4578a6958066ad2f0bb4d0218eac
n-kremeris <n.kremeris@live.com>
2021-09-13 21:07:04 +0100

    Add comments to transactional testbench

--- tb_alu.cpp
+++ tb_alu.cpp
@@ -14,7 +14,7 @@
 vluint64_t sim_time = 0;
 vluint64_t posedge_cnt = 0;
 
-
+// ALU input interface transaction item class
 class AluInTx {
     public:
         uint32_t a;
@@ -26,36 +26,48 @@ class AluInTx {
         } op;
 };
 
+// ALU output interface transaction item class
 class AluOutTx {
     public:
         uint32_t out;
 };
 
+// ALU scoreboard
 class AluScb {
     private:
         std::deque<AluInTx*> in_q;
         std::deque<AluOutTx*> out_q;
 
     public:
+        // Input interface monitor port
         void writeIn(AluInTx *tx){
+            // Push the received transaction item into a queue for later
             in_q.push_back(tx);
         }
 
+        // Output interface monitor port
         void writeOut(AluOutTx* tx){
+            // We should never get any data from the output interface
+            // before an input gets driven to the input interface
             if(in_q.empty()){
                 std::cout <<"Fatal Error in AluScb: empty AluInTx queue" << std::endl;
                 exit(1);
             }
 
+            // Grab the transaction item from the front of the input item queue
             AluInTx* in;
             in = in_q.front();
             in_q.pop_front();
+
             switch(in->op){
+                // A valid signal should not be created at the output when there is no operation,
+                // so we should never get a transaction item where the operation is NOP
                 case AluInTx::nop :
                     std::cout << "Fatal error in AluScb, received NOP on input" << std::endl;
                     exit(1);
                     break;
 
+                // Received transaction is add
                 case AluInTx::add :
                     if (in->a + in->b != tx->out) {
                         std::cout << std::endl;
@@ -66,6 +78,7 @@ class AluScb {
                     }
                     break;
 
+                // Received transaction is sub
                 case AluInTx::sub :
                     if (in->a - in->b != tx->out) {
                         std::cout << std::endl;
@@ -76,11 +89,14 @@ class AluScb {
                     }
                     break;
             }
+            // As the transaction items were allocated on the heap, it's important
+            // to free the memory after they have been used
             delete in;
             delete tx;
         }
 };
 
+// ALU input interface driver
 class AluInDrv {
     private:
         Valu *dut;
@@ -91,17 +107,20 @@ class AluInDrv {
 
         void drive(AluInTx *tx){
             if (tx->op != AluInTx::nop) {
+                // If the operation is not a NOP, we drive it onto the
+                // input interface pins
                 dut->in_valid = 1;
                 dut->op_in = tx->op;
                 dut->a_in = tx->a;
                 dut->b_in = tx->b;
             } else {
+                // If the operation is NOP, then input is not valid
                 dut->in_valid = 0;
             }
         }
 };
 
-
+// ALU input interface monitor
 class AluInMon {
     private:
         Valu *dut;
@@ -114,15 +133,21 @@ class AluInMon {
 
         void monitor(){
             if (dut->in_valid == 1) {
+                // If there is valid data at the input interface,
+                // create a new AluInTx transaction item and populate
+                // it with data observed at the interface pins
                 AluInTx *tx = new AluInTx();
                 tx->op = AluInTx::Operation(dut->op_in);
                 tx->a = dut->a_in;
                 tx->b = dut->b_in;
+
+                // then pass the transaction item to the scoreboard
                 scb->writeIn(tx);
             }
         }
 };
 
+// ALU input interface monitor
 class AluOutMon {
     private:
         Valu *dut;
@@ -135,13 +160,22 @@ class AluOutMon {
 
         void monitor(){
             if (dut->out_valid == 1) {
+                // If there is valid data at the output interface,
+                // create a new AluOutTx transaction item and populate
+                // it with result observed at the interface pins
                 AluOutTx *tx = new AluOutTx();
                 tx->out = dut->out;
+
+                // then pass the transaction item to the scoreboard
                 scb->writeOut(tx);
             }
         }
 };
 
+// ALU random transaction generator
+// This will allocate memory for an AluInTx
+// transaction item, randomise the data, and
+// return a pointer to the transaction item object
 AluInTx* rndAluInTx(){
     AluInTx *tx = new AluInTx();
     tx->op = AluInTx::Operation(rand() % 3); // Our ENUM only has entries with values 0, 1, 2
@@ -173,6 +207,8 @@ int main(int argc, char** argv, char** env) {
     m_trace->open("waveform.vcd");
 
     AluInTx   *tx;
+
+    // Here we 'instantiate' the driver, scoreboard, input and output monitor blocks
     AluInDrv  *drv    = new AluInDrv(dut);
     AluScb    *scb    = new AluScb();
     AluInMon  *inMon  = new AluInMon(dut, scb);
@@ -183,15 +219,28 @@ int main(int argc, char** argv, char** env) {
         dut->clk ^= 1;
         dut->eval();
 
+        // Do all the driving/monitoring on a positive edge
         if (dut->clk == 1){
+            // the in_valid signal is always deasserted at the start of the positive edge
+            // and is reasserted by the driver if required later
             dut->in_valid = 0;
             if (sim_time >= VERIF_START_TIME) {
+                // Generate a randomised transaction item of type AluInTx
                 tx = rndAluInTx();
+
+                // Pass the transaction item to the ALU input interface driver,
+                // which drives the input interface based on the info in the
+                // transaction item
                 drv->drive(tx);
+
+                // Monitor the input interface
                 inMon->monitor();
+
+                // Monitor the output interface
                 outMon->monitor();
             }
         }
+        // end of positive edge processing
 
         m_trace->dump(sim_time);
         sim_time++;
